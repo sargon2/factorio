8cba13a54a87e301be366fbc3fa2e7cca2a88838
David Besen <dbesen@gmail.com>
2024-11-07 10:18:26 -0700

    Update deps, and work on decoding space age

--- update_backup_from_clipboard.sh
+++ update_backup_from_clipboard.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -ex
 
-BP_STORAGE="/mnt/c/Users/dbese/AppData/Roaming/Factorio/blueprint-storage-2.dat"
-
 # Fatul reads the old blueprints to try to minimize the diff.  That's WAY too stateful. (I got a weird error when it was doing the diff.)
 # So, to prevent that, just clear them out first to ensure they're stateless.
 rm -rf blueprints/
 
-./factorio-blueprint-decoder/decode $BP_STORAGE | ./factorio-blueprint-decoder/encode-export-string | ./fatul/decode blueprints -
+./fatul/decode blueprints
